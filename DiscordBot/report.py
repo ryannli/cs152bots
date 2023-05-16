e0532b91afce33ab2704dc2621f9ca1a8e402a09
Ran Li <ryannli1129@gmail.com>
2023-05-15 19:21:17 -0700

    add basic reporting flow

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -7,11 +7,28 @@ class State(Enum):
     AWAITING_MESSAGE = auto()
     MESSAGE_IDENTIFIED = auto()
     REPORT_COMPLETE = auto()
+    SELECT_SPAM = auto()
+    SELECT_OFFENSIVE = auto()
+    SELECT_HARASSMENT = auto()
+    SELECT_ILLEGAL = auto()
+    SELECT_IMMINENT = auto()
+    ASK_TO_BLOCK_SENDER = auto()
+    BLOCK_SENDER_IDENTIFIED = auto()
+
 
 class Report:
     START_KEYWORD = "report"
     CANCEL_KEYWORD = "cancel"
     HELP_KEYWORD = "help"
+    SPAM_KEYWORD = "spam"
+    OFFENSIVE_KEYWORD = "offensive content"
+    HARASSMENT_KEYWORD = "harassment"
+    ILLEGAL_KEYWORD = "illegal content"
+    DANGER_KEYWORD = "imminent danger"
+    BULLYING_KEYWORD = "bullying"
+    UNWANTED_SEXUAL_KEYWORD = "unwanted sexual content"
+    PRIVITE_KEYWORD = "revealing private information"
+    HATE_SPEECH_KEYWORD = "hate speech targeting me"
 
     def __init__(self, client):
         self.state = State.REPORT_START
@@ -55,12 +72,53 @@ class Report:
 
             # Here we've found the message - it's up to you to decide what to do next!
             self.state = State.MESSAGE_IDENTIFIED
-            return ["I found this message:", "```" + message.author.name + ": " + message.content + "```", \
-                    "This is all I know how to do right now - it's up to you to build out the rest of my reporting flow!"]
+            reply = "I found this message: ```" + message.author.name + ": " + message.content + "```\n"
+            reply += "Select your reason for reporting this message:\n"
+            reply += f"  `{Report.SPAM_KEYWORD}`\n"
+            reply += f"  `{Report.OFFENSIVE_KEYWORD}`\n"
+            reply += f"  `{Report.HARASSMENT_KEYWORD}`\n"
+            reply += f"  `{Report.ILLEGAL_KEYWORD}`\n"
+            reply += f"  `{Report.DANGER_KEYWORD}`\n"
+            return [reply]
         
         if self.state == State.MESSAGE_IDENTIFIED:
-            return ["<insert rest of reporting flow here>"]
+            if (Report.SPAM_KEYWORD in message.content):
+                self.state = State.SELECT_SPAM
+                return ["Thank you for helping to keep our platform safe. We will investigate this report. "]
+            if (Report.OFFENSIVE_KEYWORD in message.content):
+                self.state = State.SELECT_OFFENSIVE
+                return ["Thank you for reporting. We will investigate to determine whether this content violates our policies. "]
+            if (Report.HARASSMENT_KEYWORD in message.content):
+                self.state = State.SELECT_HARASSMENT
+                reply = "Thank you for reporting. What type of harassment is this? \n"
+                reply += f"  `{Report.BULLYING_KEYWORD}`\n"
+                reply += f"  `{Report.UNWANTED_SEXUAL_KEYWORD}`\n"
+                reply += f"  `{Report.PRIVITE_KEYWORD}`\n"
+                reply += f"  `{Report.HATE_SPEECH_KEYWORD}`\n"
+                return [reply]
+            if (Report.ILLEGAL_KEYWORD in message.content):
+                self.state = State.SELECT_ILLEGAL
+                return ["Thank you for reporting. We will investigate to determine whether this content warrants removal and/or referral to law enforcement. "]
+            if (Report.DANGER_KEYWORD in message.content):
+                self.state = State.SELECT_IMMINENT
+                return ["Thank you for reporting. We take threats to people’s safety very seriously and our moderation team will review this report. If you believe you are in immediate danger, you should also contact local law enforcement."]
+            return ["Wrong input. Please select the reason again."]
+        
+        if self.state == State.SELECT_HARASSMENT:
+            replies = []
+            if (Report.BULLYING_KEYWORD in message.content or Report.UNWANTED_SEXUAL_KEYWORD in message.content or 
+                Report.PRIVITE_KEYWORD in message.content or Report.HATE_SPEECH_KEYWORD in message.content):
+                replies.append("Thank you for reporting. We will investigate to determine whether this content warrants removal and/or referral to law enforcement.")
+                replies.append("Would you like to hide messages from this sender (Reply `Yes`/`No`)? They will not know this has happened.")
+                self.state = State.ASK_TO_BLOCK_SENDER
+            else:
+                replies.append("Wrong input. Please select the type of harassment again.")
+            return replies
 
+        if self.state == State.ASK_TO_BLOCK_SENDER:
+            if ("Yes" in message.content):
+                self.state = State.BLOCK_SENDER_IDENTIFIED
+                return ["This sender is blocked! (simulated blocking)"]
         return []
 
     def report_complete(self):
